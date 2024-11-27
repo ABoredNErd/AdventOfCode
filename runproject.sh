389a34421358eb245ad35fc45e22abed4d6d3967
ABoredNErd <charliewordley@gmail.com>
2024-11-27 17:21:10 +0000

    Added class for day1 year 2. Also updated runproject.sh to only clean if asked

--- runproject.sh
+++ runproject.sh
@@ -1,2 +1,14 @@
-mvn clean install
+clean=false
+for command in $@; do
+    if [ $command = "clean" ] ; then
+        clean=true
+    fi
+done
+
+if [ $clean = true ] ; then
+    mvn clean install
+else
+    mvn install
+fi
+
 java -jar target/advent-0.1.0.jar
--- src/main/java/advent/AdventOfCode.java
+++ src/main/java/advent/AdventOfCode.java
@@ -1,26 +1,15 @@
 package advent;
 
-import advent.days.*;
 import advent.fileReader.FileReader;
+import advent.year2.*;
 
 public class AdventOfCode {
-
-    class Hand{
-        public int power;
-        public String hand;
-        public int bet;
-        public int rank;
-
-        public Hand(){
-        }
-    }
 
     public static void main(String[] args){
         FileReader reader = new FileReader("inputFiles/Day7.txt");
             
-            
         long startTime = System.currentTimeMillis();
-        new Day7(reader.fileContents());
+        new day1(reader.fileContents());
         long endTime = System.currentTimeMillis();
         System.out.println("Run time in Millis: " + (endTime - startTime));
     }
